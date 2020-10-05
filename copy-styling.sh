3aecb4a0537c7932256c2495660b94a99ab5b965
Radovan Bast <bast@users.noreply.github.com>
2020-10-05 17:52:24 +0200

    it seems symlinks cannot be used here

--- copy-styling.sh
+++ copy-styling.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 for section in motivation design data-formats tools publications gallery; do
-    ln -sf slides.css $section.css
-    ln -sf slides.body.html $section.body.html
+    cp slides.css $section.css
+    cp slides.body.html $section.body.html
 done
